d52e22c7def392b545184dbe2f4f7bc9e6aa97b2
Hojun Im <ghwnsdla8094@gmail.com>
2024-12-18 02:09:46 +0900

    [Chore] .gitignore 및 설정 파일 추가
    - .gitignore 파일 추가
    - application.yml 설정 추가
    - build.gradle.kts 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     compileOnly("org.projectlombok:lombok")
